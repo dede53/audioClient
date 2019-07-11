3f8d8475efa6a02f989276ca3feccf346af56067
dede53 <hallomarcel.hm@gmail.com>
2019-07-11 22:53:51 +0200

    add daemon

---
+++ daemon.js
@@ -0,0 +1,16 @@
+var daemon = require("daemonize2").setup({
+    main: "expClient.js",
+    name: "audioClient",
+    pidfile: "audioClient.pid"
+});
+ 
+switch (process.argv[2]) {
+    case "start":
+        daemon.start();
+        break;
+    case "stop":
+        daemon.stop();
+        break;
+    default:
+        console.log("Usage: [start|stop]");
+}
